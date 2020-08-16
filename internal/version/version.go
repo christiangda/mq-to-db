1efdf5335dcf282f3a1ea01ae43ed34270494458
Christian González Di Antonio <christian.gonzalez@alea.com>
2020-08-16 10:56:09 +0200

    improved version

--- internal/version/version.go
+++ internal/version/version.go
@@ -5,7 +5,13 @@ import (
 	"runtime"
 )
 
-// Build information. Populated at build-time.
+// Populated at build-time.
+// go build -ldflags \
+// “-X github.com/mq-to-db/internal/verion.Version=$(git rev-parse --abbrev-ref HEAD) \
+// -X github.com/mq-to-db/internal/verion.Revision=$(git log –pretty=format:’%h’ -n 1) \
+// -X github.com/mq-to-db/internal/verion.Branch=$(git log –pretty=format:’%h’ -n 1) \
+// -X github.com/mq-to-db/internal/verion.BuildUser=$(git log –pretty=format:’%h’ -n 1) \
+// -X github.com/mq-to-db/internal/verion.BuildDate=$(date +”%Y-%m-%dT%H:%M:%S”)”
 var (
 	Version   string
 	Revision  string
@@ -15,12 +21,18 @@ var (
 	GoVersion = runtime.Version()
 )
 
-// Info returns version, branch and revision information.
-func Info() string {
-	return fmt.Sprintf("(version=%s, branch=%s, revision=%s)", Version, Branch, Revision)
+// GetVersion returns application version
+func GetVersion() string {
+	return Version
 }
 
-// BuildContext returns goVersion, buildUser and buildDate information.
-func BuildContext() string {
-	return fmt.Sprintf("(go=%s, user=%s, date=%s)", GoVersion, BuildUser, BuildDate)
+// GetVerionExtended returns application version, branch and revision information.
+func GetVerionExtended() string {
+	return fmt.Sprintf("(version=%s, branch=%s, revision=%s, go=%s, user=%s, date=%s)",
+		Version,
+		Branch,
+		Revision,
+		GoVersion,
+		BuildUser,
+		BuildDate)
 }
